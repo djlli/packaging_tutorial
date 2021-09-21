3dbefa37834ecf8fc0203042773e24822345503d
djlli <jkjaerh@gmail.com>
2021-09-21 21:26:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example_pkg",
+    name="examplepkg",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
